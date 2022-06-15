1275a97975196c4451fd8912bac1827b637838d2
Welly Immanuel <welly.immanuel@aladinbank.id>
2022-06-15 15:29:30 +0700

    fix ui finish

--- src/page/MainMenu.tsx
+++ src/page/MainMenu.tsx
@@ -2,15 +2,23 @@ import React from "react";
 import { Card, Container, Button, Row, Col, Image} from "react-bootstrap";
 import {Link} from "react-router-dom";
 import { Header } from "../components/header";
+import { BiLogIn } from 'react-icons/bi';
 
 export function MainMenu() {
     return (
         <>
-            <Header/>
+            <div className="d-flex justify-content-around">
+            <div><Header/></div>
+            <div className="w-25 h-25 text-end">
+                <Link to ="/login" className="text-dark text-end w-25 h-25"><BiLogIn className="h-50 w-25" type="button" /></Link>
+            </div>
+            
+            </div>
+            
             <Container style={{width: "70%"}} className="position-absolute top-50 start-50 translate-middle ">
                 <Row>
                     <Col>
-                        <Link to="/oboarding" className="d-block text-decoration-none text-body text-center">
+                        <Link to="/onboarding" className="d-block text-decoration-none text-body text-center">
                             <Image src="https://cdn-icons-png.flaticon.com/512/2534/2534204.png" className="rounded mx-auto d-block w-75" />
                             <p className="mt-3">Onboarding</p>
                         </Link>
@@ -22,9 +30,9 @@ export function MainMenu() {
                         </Link>
                     </Col>
                     <Col>
-                        <Link to="/payment" className="d-block text-decoration-none text-body text-center">
+                        <Link to="/repayment" className="d-block text-decoration-none text-body text-center">
                             <Image src="https://cdn-icons-png.flaticon.com/512/1086/1086741.png" className="rounded mx-auto d-block w-75" />
-                            <p className="mt-3">Payment</p>
+                            <p className="mt-3">Re-Payment</p>
                             
                         </Link>
                     </Col>
--- src/page/preview.tsx
+++ src/page/preview.tsx
@@ -1,12 +1,14 @@
 import React from "react";
-import { Tabs, Tab, Container, Card} from "react-bootstrap";
+import { Tabs, Tab, Container, Card, Table, Button} from "react-bootstrap";
+import { Header } from "../components/header";
+import { Link } from "react-router-dom";
+import { MdArrowBack } from 'react-icons/md';
 
 export function Preview() {
     return (
         <>
-            <Container style={{width: "70%", height: "80%"}} className="position-absolute top-50 start-50 translate-middle ">
-                <Card>
-                    <Card.Body className="d-grid gap-2">
+            <Header/>
+            <Container style={{width: "70%", height: "80%"}} className="position-absolute top-50 start-50 translate-middle mt-5">
                         <Tabs
                         defaultActiveKey="CIFA"
                         transition={true}
@@ -14,9 +16,42 @@ export function Preview() {
                         className="mb-3"
                         >
                             <Tab eventKey="CIFA" title="Onboarding - CIF & Account">
-                                <>
-                                Haloo
-                                </>
+                            <Table bordered hover>
+                                <thead>
+                                    <tr>
+                                        <th>Name</th>
+                                        <th>Age</th>
+                                        <th>Nick Name</th>
+                                        <th>Approval</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td>Giacomo Guilizzoni</td>
+                                        <td>40</td>
+                                        <td>Peldi</td>
+                                        <td className="d-flex justify-content-evenly">
+                                            <Button variant="primary">Accept</Button>  <Button variant="danger">Reject</Button>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td>Marco Botton</td>
+                                        <td>38</td>
+                                        <td></td>
+                                        <td className="d-flex justify-content-evenly">
+                                            <Button variant="primary">Accept</Button>  <Button variant="danger">Reject</Button>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td>Mariah Maclachlan</td>
+                                        <td>41</td>
+                                        <td>Potata</td>
+                                        <td className="d-flex justify-content-evenly">
+                                            <Button variant="primary">Accept</Button>  <Button variant="danger">Reject</Button>
+                                        </td>
+                                    </tr>
+                                </tbody>
+                                </Table>
                             </Tab>
                             <Tab eventKey="CA" title="Onboarding - Credit Arrangement">
                             <>
@@ -34,9 +69,7 @@ export function Preview() {
                                 </>
                             </Tab>
                         </Tabs>
-                    </Card.Body>
-                </Card>
-
+                        <Link to ="/home" className="text-dark text-start d-inline-block w-25 mt-5"><MdArrowBack className="h-50 w-25" type="button" /></Link>
             </Container>
         </>
     );
